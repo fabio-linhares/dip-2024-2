2d159838add235f1adfc79a8477a76769882731d
Fábio Linhares <fl@ic.ufal.br>
2025-02-23 13:05:20 -0300

    Adiciona variávies, implementa condição, comentários e cálculos de estatísticas para a função get_image_info

--- tasks/task-03-get-image-info/task-03-get_image_info.py
+++ tasks/task-03-get-image-info/task-03-get_image_info.py
@@ -12,7 +12,32 @@ def get_image_info(image):
     """
     
     ### START CODE HERE ###
-    ### TODO
+
+    # Dimensões:
+    # Para imagens cinza: altura e largura;
+    # Para imagens coloridas: altura, largura e profundidade;
+
+    height, width = image.shape[:2] # [:2] retorna os dois primeiros elementos desta tupla, que sempre correspondem à altura e largura
+    dtype = str(image.dtype) # retorna o tipo de dados do array: uint8 ou float32, por exemplo.
+    
+    # Profundidade: refere-se ao número de canais de cor em uma imagem:
+    # Profundidade 1: Imagens em escala de cinza (um único canal).
+    # Profundidade 3: Imagens coloridas típicas (RGB - Red, Green, Blue).
+    # Profundidade 4: Imagens com canal alfa (RGBA - Red, Green, Blue, Alpha).
+
+    # image.ndim retorna o número de dimensões do array.
+    if image.ndim == 2: # é uma imagem em escala de cinza (profundidade 1).
+        depth = 1
+    elif image.ndim == 3:  # é uma imagem colorida.
+        depth = image.shape[2] # dá o número de canais.
+    else:
+        depth = None
+    
+    # Estatísticas: fornecem informações sobre a distribuição dos valores dos pixels:
+    min_val = np.min(image)
+    max_val = np.max(image)
+    mean_val = np.mean(image)
+    std_val = np.std(image)
     ### END CODE HERE ###
 
     return {
